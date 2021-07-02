8739656bd24a4d879b182beb58b3ac3078ad8578
AdarshNaidu <naidu.adarsh2511@gmail.com>
2021-07-02 21:56:08 +0530

    printing all runes from input query

--- main.go
+++ main.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"github.com/AdarshNaidu/simple-sql-parser/sql"
 )
 
 func main() {
 	fmt.Println("Hello Parser")
+
+	var query string = "Hello"
+
+	scanner := sql.NewScanner(strings.NewReader(query))
+
+	fmt.Println(scanner)
+
+	// print all characters of the input.
+	for {
+		ch, _, err := scanner.R.ReadRune()
+
+		if err != nil {
+			break
+		}
+
+		fmt.Println(string(ch))
+	}
 }
 
