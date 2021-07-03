e1767ee6870111bb2a6919d0fe28974bf40265b7
AdarshNaidu <naidu.adarsh2511@gmail.com>
2021-07-03 21:34:52 +0530

    adding read and unread scanner methods

--- sql/scanner.go
+++ sql/scanner.go
@@ -5,13 +5,27 @@ import (
 	"io"
 )
 
+const eof = rune(0)
+
 // lexical scanner object
 type Scanner struct {
-	R *bufio.Reader
+	r *bufio.Reader
 }
 
 // scanner constructor, can take input from any io.Reader interface
 func NewScanner(r io.Reader) *Scanner {
 	return &Scanner{bufio.NewReader(r)}
 }
+
+// read and return the next rune
+func (s *Scanner) read() rune {
+	ch, _, err := s.r.ReadRune()
+	if err != nil {
+		return eof
+	}
+	return ch
+}
+
+func (s *Scanner) unread() { _ = s.r.UnreadRune() }
+
 
