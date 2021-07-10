3d633a295a635d57a5af6a911e01121b2eaa0ef5
AdarshNaidu <naidu.adarsh2511@gmail.com>
2021-07-10 16:13:15 +0530

    parsing select statement

--- sql/parser.go
+++ sql/parser.go
@@ -1,8 +1,15 @@
 package sql
 
 import (
+	"fmt"
 	"io"
 )
+
+type SelectStatement struct {
+	Fields []string
+	TableName string
+}
+
 type Parser struct {
 	s *Scanner
 	buf struct {
@@ -15,6 +22,45 @@ type Parser struct {
 func NewParser(r io.Reader) *Parser {
 	return &Parser{s: NewScanner(r)}
 }
+
+func (p *Parser) Parse() (*SelectStatement, error) {
+	stmt := &SelectStatement{}
+
+	// first token should be select
+	if tok, lit := p.scanIgnoreWhitespace(); tok != SELECT {
+		return nil, fmt.Errorf("found %q, expected SELECT", lit)
+	}
+
+	// after select should be fields or asterisk
+	for {
+		tok, lit := p.scanIgnoreWhitespace()
+		if(tok != IDENT && tok != ASTERISK) {
+			return nil, fmt.Errorf("found %q, expected field", lit)
+		}
+
+		stmt.Fields = append(stmt.Fields, lit)
+
+		if tok, _ := p.scanIgnoreWhitespace(); tok != COMMA {
+			p.unscan()
+			break
+		}
+	}
+
+	// next should be from 
+	if tok, lit := p.scanIgnoreWhitespace(); tok != FROM {
+		return nil, fmt.Errorf("found %q, expected FROM", lit)
+	}
+
+	// table name
+	tok, lit := p.scanIgnoreWhitespace()
+	if tok != IDENT {
+		return nil, fmt.Errorf("found %q, expected table name", lit)
+	}
+	stmt.TableName = lit
+
+	return stmt, nil
+}
+
 // ignore the white space and return the next token
 func (p *Parser) scanIgnoreWhitespace() (tok Token, lit string) {
 	tok, lit = p.scan()
