dc894979a84f580bdfa07bc04548689c16abaea1
AdarshNaidu <naidu.adarsh2511@gmail.com>
2021-07-11 21:12:00 +0530

    taking input from user

--- main.go
+++ main.go
@@ -2,9 +2,33 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"bufio"
+	"os"
+
+	"github.com/AdarshNaidu/simple-sql-parser/sql"
 )
 
 func main() {
-	fmt.Println("Hello Parser")
+	
+	scanner := bufio.NewScanner(os.Stdin)
+	
+	fmt.Println("Please enter an sql query: ")
+
+	for scanner.Scan() {
+
+		parser := sql.NewParser(strings.NewReader(scanner.Text()))
+
+		result, err := parser.Parse()
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Printf("Result \n%s", result)
+		}
+
+		fmt.Println("\nPlease enter an sql query: ")
+		
+	}
 }
 
