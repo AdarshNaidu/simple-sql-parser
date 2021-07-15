76245d085c885e222a3e951c0179a6c0f8164e8a
AdarshNaidu <naidu.adarsh2511@gmail.com>
2021-07-15 21:58:43 +0530

    moving parse insert function into separate file

--- sql/parser.go
+++ sql/parser.go
@@ -5,28 +5,6 @@ import (
 	"io"
 )
 
-type InsertStatement struct {
-	Fields []string
-	Values []string
-	TableName string
-}
-
-func (s InsertStatement) String() string {
-	var fields string
-
-	for _, field := range s.Fields {
-		fields += field + " "
-	}
-
-	var values string
-
-	for _, value := range s.Values {
-		values += value + " "
-	}
-
-	return fmt.Sprintf("Fields: %s\nValues: %s\nTable Name: %s\n", fields, values, s.TableName)
-}
-
 type Parser struct {
 	s *Scanner
 	buf struct {
@@ -51,89 +29,6 @@ func (p *Parser) Parse() (interface{}, error) {
 		return p.parseInsert()
 	}
 	return nil, fmt.Errorf("Sorry, only SELECT and INSERT is supported.")
-}
-
-func (p *Parser) parseInsert() (*InsertStatement, error) {
-	stmt := &InsertStatement{}
-
-	// first token should be INSERT
-	if tok, lit := p.scanIgnoreWhitespace(); tok != INSERT {
-		return nil, fmt.Errorf("found %q, expected INSERT", lit)
-	}
-
-	// second token should be into
-	if tok, lit := p.scanIgnoreWhitespace(); tok != INTO {
-		return nil, fmt.Errorf("found %q, expected INTO", lit)
-	}
-
-	// third token table name
-	tok, lit := p.scanIgnoreWhitespace();
-	if tok != IDENT {
-		return nil, fmt.Errorf("found %q, expected table name", lit)
-	}
-	stmt.TableName = lit
-
-	if tok, lit := p.scanIgnoreWhitespace(); tok != OPENING_BRACKET {
-		return nil, fmt.Errorf("found %q, expect (", lit)
-	}
-
-	for {
-		tok, lit := p.scanIgnoreWhitespace()
-
-		if(tok == CLOSING_BRACKET) { 
-			p.unscan()
-			break 
-		}
-
-		if(tok != IDENT) {
-			return nil, fmt.Errorf("found %q, expected field name or )", lit)
-		}
-
-		stmt.Fields = append(stmt.Fields, lit)
-
-		if tok, _ := p.scanIgnoreWhitespace(); tok != COMMA {
-			p.unscan()
-			break
-		}
-	}
-
-	if tok, lit := p.scanIgnoreWhitespace(); tok != CLOSING_BRACKET {
-		return nil, fmt.Errorf("found %q, expected )", lit)
-	}
-
-	if tok, lit := p.scanIgnoreWhitespace(); tok != VALUES {
-		return nil, fmt.Errorf("found %q, expected VALUES", lit)
-	}
-
-	if tok, lit := p.scanIgnoreWhitespace(); tok != OPENING_BRACKET {
-		return nil, fmt.Errorf("found %q, expect (", lit)
-	}
-	
-	for {
-		tok, lit := p.scanIgnoreWhitespace()
-
-		if(tok == CLOSING_BRACKET) { 
-			p.unscan()
-			break 
-		}
-
-		if(tok != IDENT) {
-			return nil, fmt.Errorf("found %q, expected value or )", lit)
-		}
-
-		stmt.Values = append(stmt.Values, lit)
-
-		if tok, _ := p.scanIgnoreWhitespace(); tok != COMMA {
-			p.unscan()
-			break
-		}
-	}
-
-	if tok, lit := p.scanIgnoreWhitespace(); tok != CLOSING_BRACKET {
-		return nil, fmt.Errorf("found %q, expected )", lit)
-	}
-
-	return stmt, nil
 }
 
 // ignore the white space and return the next token
